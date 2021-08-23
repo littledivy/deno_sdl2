59bdadf848e94bbd1d17b81215b233bd51c21fc2
Divy Srivastava <dj.srivastava23@gmail.com>
2021-08-23 21:37:41 +0530

    fix

--- src/canvas.ts
+++ src/canvas.ts
@@ -390,14 +390,14 @@ async function downloadRelease() {
 async function init(
   cb: (conn: Deno.Conn) => Promise<void>,
   // TODO(@littledivy): Make this toggleable with a build script?
-  dev: boolean = true,
+  dev: boolean = false,
 ) {
   if (!dev) await downloadRelease();
   const listener = Deno.listen({ port: 34254, transport: "tcp" });
   const process = Deno.run({
     cmd: [dev ? "target/debug/deno_sdl2" : "./deno_sdl2"],
     stderr: "inherit",
-    stdout: "inherit",  
+    stdout: "inherit",
   });
   const conn = await listener.accept();
   const reqBuf = await readStatus(conn);
