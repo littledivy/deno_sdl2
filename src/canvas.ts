f6735d6a072ad50dd10a152ad0b2f3459c1fb026
breadA <62501544+dhairy-online@users.noreply.github.com>
2021-08-24 22:27:31 +0530

    chore: add documentation (#27)

--- src/canvas.ts
+++ src/canvas.ts
@@ -98,60 +98,89 @@ export class Canvas extends EventEmitter<WindowEvent> {
           false;
       });
   }
-
+  /**
+   * Updates the screen with any rendering performed since the previous call.
+   * */
   present() {
     this.#tasks.push("present");
   }
-
+  /**
+   * Clears the current rendering target with the drawing color.
+   * */
   clear() {
     this.#tasks.push("clear");
   }
-
+  /**
+   * Sets the color used for drawing operations (rect, line and clear).
+   * */
   setDrawColor(r: number, g: number, b: number, a: number) {
     this.#tasks.push({ setDrawColor: { r, g, b, a } });
   }
-
+  /**
+   * Sets the drawing scale for rendering on the current target.
+   * */
   setScale(x: number, y: number) {
     this.#tasks.push({ setScale: { x, y } });
   }
-
+  /**
+   * Draws a point on the current rendering target.
+   * */
   drawPoint(x: number, y: number) {
     this.#tasks.push({ drawPoint: { x, y } });
   }
-
+  /**
+   * Draws multiple points on the current rendering target.
+   * */
   drawPoints(points: Point[]) {
     this.#tasks.push({ drawPoints: { points } });
   }
-
+  /**
+   * Draws a line on the current rendering target. Errors if drawing fails for any reason.
+   * */
   drawLine(p1: Point, p2: Point) {
     this.#tasks.push({ drawLine: { p1, p2 } });
   }
-
+  /**
+   * Draws a series of connected lines on the current rendering target. Errors if drawing fails for any reason.
+   * */
   drawLines(points: Point[]) {
     this.#tasks.push({ drawLines: { points } });
   }
-
+  /**
+   * Draws a rectangle on the current rendering target. Errors if drawing fails for any reason.
+   * */
   drawRect(x: number, y: number, width: number, height: number) {
     this.#tasks.push({ drawRect: { x, y, width, height } });
   }
-
+  /**
+   * Draws some number of rectangles on the current rendering target.
+   * Errors if drawing fails for any reason.
+   * */
   drawRects(rects: Rect[]) {
     this.#tasks.push({ drawRects: { rects } });
   }
-
+  /**
+   * Fills a rectangle on the current rendering target with the drawing color.
+   * Passing None will fill the entire rendering target. Errors if drawing fails for any reason.
+   * */
   fillRect(x: number, y: number, width: number, height: number) {
     this.#tasks.push({ fillRect: { x, y, width, height } });
   }
-
+  /**
+   * Fills some number of rectangles on the current rendering target with the drawing color.
+   * Errors if drawing fails for any reason
+   * */
   fillRects(rects: Rect[]) {
     this.#tasks.push({ fillRects: { rects } });
   }
-
+  /**
+   * Exit from canvas.
+   * */
   quit() {
     this.#tasks.push("quit");
     this.#closed = true;
   }
-
+  // TODO: (dhairy-online)
   setDisplayMode(
     width: number,
     height: number,
@@ -160,11 +189,15 @@ export class Canvas extends EventEmitter<WindowEvent> {
   ) {
     this.#tasks.push({ setDisplayMode: { width, height, rate, format } });
   }
-
+  /**
+   * Set title of the canvas.
+   * */
   setTitle(title: string) {
     this.#tasks.push({ setTitle: { title } });
   }
-
+  /**
+   * Use this function to set the icon for a window.
+   * */
   setIcon(icon: string) {
     this.#tasks.push({ setIcon: { icon } });
   }
@@ -185,6 +218,11 @@ export class Canvas extends EventEmitter<WindowEvent> {
     this.#tasks.push({ setBrightness: { brightness } });
   }
 
+  /**
+   * Set the transparency of the window. The given value will be clamped internally between 0.0 (fully transparent),
+   * and 1.0 (fully opaque).
+   * This method returns an error if opacity isn't supported by the current platform.
+   * */
   setOpacity(opacity: number) {
     this.#tasks.push({ setOpacity: { opacity } });
   }
@@ -213,6 +251,13 @@ export class Canvas extends EventEmitter<WindowEvent> {
     this.#tasks.push("restore");
   }
 
+  /**
+   * Load a font for rendering.
+   * @param path Relative path to the font
+   * @param size Size of the font. (eg: 128)
+   * @param opts Font options (eg: { italics: true })
+   * @returns A loaded font reference
+   */
   loadFont(path: string, size: number, opts?: { style: string }): number {
     const options = { path, size, ...opts };
     const index = this.#fonts.push(options);
@@ -220,6 +265,13 @@ export class Canvas extends EventEmitter<WindowEvent> {
     return index;
   }
 
+  /**
+   * Render a loaded font onto the current rendering target.
+   * @param font a font loaded with `loadFont`
+   * @param text Text to render.
+   * @param options Font rendering options.
+   * @param target Portion of the current rendering target.
+   */
   renderFont(
     font: number,
     text: string,
@@ -233,6 +285,10 @@ export class Canvas extends EventEmitter<WindowEvent> {
     this.#tasks.push({ renderFont: { font, text, options, target, ..._font } });
   }
 
+  /**
+   * Set the cursor icon.
+   * @param path Path to the source file
+   */
   setCursor(path: string) {
     const index = this.#resources.push(this.#resources.length);
     this.#tasks.push({ setCursor: { path, index } });
@@ -243,28 +299,54 @@ export class Canvas extends EventEmitter<WindowEvent> {
   //   this.#audioCallback = callback;
   // }
 
+  /**
+   * Play a sound.
+   * @param path Path of the source file
+   */
   playMusic(path: string) {
     this.#tasks.push({ playMusic: { path } });
   }
 
+  /**
+   * Create a new surface.
+   * @param width width of the surface.
+   * @param height height of the surface.
+   * @param format pixel format of the surface.
+   * @returns a new surface
+   */
   createSurface(width: number, height: number, format: PixelFormat) {
     const index = this.#resources.push({ width, height, format });
     this.#tasks.push({ createSurface: { width, height, format, index } });
     return index;
   }
 
+  /**
+   * Create a new surface from bitmap
+   * @param path Path to the source file
+   * @returns a new surface
+   */
   loadBitmap(path: string) {
     const index = this.#resources.push({ path });
     this.#tasks.push({ createSurfaceBitmap: { path, index } });
     return index;
   }
 
+  /**
+   * Creates a new surface with SDL2_Image. Supports PNG and JPEG.
+   * @param path Path to the source file
+   * @returns a new surface
+   */
   loadSurface(path: string) {
     const index = this.#resources.push({ path });
     this.#tasks.push({ loadSurface: { path, index } });
     return index;
   }
 
+  /**
+   * Creates a new texture from existing surface.
+   * @param surface A surface created from load/create surface methods
+   * @returns Texture
+   */
   createTextureFromSurface(surface: number) {
     // TODO: Verify surface
     const index = this.#resources.push({ surface });
@@ -272,10 +354,22 @@ export class Canvas extends EventEmitter<WindowEvent> {
     return index;
   }
 
-  copy(texture: number, rect1: Rect, rect2: Rect) {
-    this.#tasks.push({ copyRect: { texture, rect1, rect2 } });
+  /**
+   * Copied a portion of the texture into the current rendering target.
+   * @param texture texture to be copied from
+   * @param src portion of the texture to copy.
+   * @param dest texture will be stretched on the given destination
+   */
+  copy(texture: number, src: Rect, dest: Rect) {
+    this.#tasks.push({ copyRect: { texture, rect1: src, rect2: dest } });
   }
 
+  /**
+   * Start the event. Under the hood, it fires up the Rust client, polls for events and send tasks.
+   * This function blocks rest of the JS event loop.
+   *
+   * Downloads the `deno_sdl2` client from Github releases on the first run.
+   */
   async start() {
     this.#closed = false;
     init(async (conn) => {
