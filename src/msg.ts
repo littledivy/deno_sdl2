1758ed0eacf6231fba26c2dcf05d11e8efef3919
Divy Srivastava <dj.srivastava23@gmail.com>
2021-08-28 11:25:51 +0530

    perf: reuse buffer, use Deno.core.encode/decode and drop DataView (#31)
    * optimize decodeConn
    
    * perf: reuse buffer, use Deno.core.encode/decode and drop DataView

--- src/msg.ts
+++ src/msg.ts
@@ -1,39 +1,49 @@
-const encoder = new TextEncoder();
+// @ts-ignore
+const encoder = Deno.core?.encode || new TextEncoder().encode;
+
+let encodeu8 = new Uint8Array(1024);
+const encodeu32 = new Uint32Array(encodeu8.buffer);
 
 export function encode<T>(object: T): Uint8Array {
-  const payload = encoder.encode(JSON.stringify(object));
+  // ~0.02ms
+  const payload = encoder(JSON.stringify(object));
 
-  const buf = new Uint8Array(4 + payload.length);
-  const view = new DataView(buf.buffer);
-  view.setInt32(0, payload.byteLength, true);
+  // ~0.17ms - ~0.02ms
+  const len = 4 + payload.length;
+  if (len > encodeu8.length) encodeu8 = new Uint8Array(len);
+  const buf = encodeu8.subarray(0, len);
+  encodeu32[0] = payload.byteLength;
+
   buf.set(payload, 4);
-
   return buf;
 }
 
-const decoder = new TextDecoder();
+// @ts-ignore
+const decoder = Deno.core?.decode || new TextDecoder().decode;
+
+const su8 = new Uint8Array(4);
+let last = new Uint8Array(1024);
+const su32 = new Uint32Array(su8.buffer);
 
 export async function decodeConn<T>(conn: Deno.Conn): Promise<T> {
-  // ~0.1 - 0.15ms
-  const eventLengthBuffer = new Uint8Array(4);
-  await conn.read(eventLengthBuffer);
+  // ~0.07ms
+  await conn.read(su8);
 
-  // ~0.04ms
-  const view = new DataView(eventLengthBuffer.buffer, 0);
-  const eventLength = view.getUint32(0, true);
+  // ~0.01ms
+  const eventLength = su32[0];
 
-  // ~0.05ms
-  const eventBuffer = new Uint8Array(eventLength);
-  await conn.read(eventBuffer);
+  // ~0.03ms
+  if (eventLength > last.length) last = new Uint8Array(eventLength);
+  const buf = last.subarray(0, eventLength);
+  await conn.read(buf);
 
-  // ~0.14ms
-  const event = JSON.parse(decoder.decode(eventBuffer));
-
+  // ~0.02ms
+  const event = JSON.parse(decoder(buf));
   return event as T;
 }
 
+const status = new Uint8Array(1);
 export async function readStatus(conn: Deno.Conn): Promise<number> {
-  const status = new Uint8Array(1);
   await conn.read(status);
 
   return status[0];
