4aea6c70e151c715000e100b49895fc8dda1e900
hashrock <hashrock@users.noreply.github.com>
2021-09-19 21:08:14 +0900

    add sprite example

---
+++ examples/sprite/main.ts
@@ -0,0 +1,106 @@
+import { Canvas } from "../../src/canvas.ts";
+import { drawMap, Sprite } from "./util.ts";
+
+const canvasSize = { width: 400, height: 400 };
+
+const canv = new Canvas({ title: "Deno run!", ...canvasSize });
+
+const surface = canv.loadSurface("./sprite.png");
+const texture = canv.createTextureFromSurface(surface);
+
+const map = [
+  [8, 8, 9, 8, 11, 8, 8, 8],
+  [8, 8, 8, 8, 8, 8, 8, 8],
+  [8, 10, 8, 8, 8, 8, 8, 8],
+  [8, 8, 8, 8, 8, 8, 8, 8],
+  [8, 8, 8, 8, 8, 8, 10, 8],
+  [8, 8, 8, 8, 8, 9, 8, 8],
+  [10, 8, 8, 8, 8, 8, 8, 8],
+  [8, 8, 11, 8, 8, 8, 8, 8],
+];
+
+const denoTextureFrames = [
+  { x: 0, y: 0, width: 16, height: 16 },
+  { x: 16, y: 0, width: 16, height: 16 },
+  { x: 32, y: 0, width: 16, height: 16 },
+  { x: 48, y: 0, width: 16, height: 16 },
+];
+
+function random(min: number, max: number) {
+  return (Math.random() * (max - min) + min) | 0;
+}
+
+function createDenoInstance() {
+  const deno = new Sprite(texture, denoTextureFrames);
+  deno.x = random(0, canvasSize.width);
+  deno.y = random(0, canvasSize.height);
+  deno.originX = deno.frames[0].width / 2;
+  deno.originY = deno.frames[0].height;
+  deno.scale = 4;
+  deno.vx = 2;
+  deno.vy = 1;
+  return deno;
+}
+
+const denos: Sprite[] = [];
+
+for (let i = 0; i < 1; i++) {
+  denos.push(createDenoInstance());
+}
+
+let cnt = 0;
+
+function frame() {
+  canv.clear();
+  drawMap(texture, canv, map, 16);
+
+  for (const deno of denos) {
+    deno.tick();
+    deno.draw(canv);
+
+    deno.wrap(canvasSize.width, canvasSize.height);
+
+    // make deno jump
+    deno.z = Math.abs(Math.sin(cnt / 10) * 16) | 0;
+
+    if((cnt / 20 | 0) % 2 === 0) {
+      if (deno.vx > 0) {
+        deno.index = 2;
+      } else {
+        deno.index = 0;
+      }
+    }else{
+      if (deno.vx > 0) {
+        deno.index = 3;
+      } else {
+        deno.index = 1;
+      }
+
+    }
+
+    cnt++;
+  }
+
+  canv.present();
+  Deno.sleepSync(10);
+}
+
+for await (const event of canv) {
+  switch (event.type) {
+    case "draw":
+      frame();
+      break;
+    case "quit":
+      canv.quit();
+      break;
+    case "mouse_motion":
+      // Mouse stuff
+      break;
+    case "key_down":
+      // Keyboard stuff
+      break;
+    // ...
+    default:
+      break;
+  }
+}
---
+++ examples/sprite/util.ts
@@ -0,0 +1,77 @@
+import { Canvas, Rect } from "../../src/canvas.ts";
+
+export function drawMap(
+  texture: number,
+  canvas: Canvas,
+  map: number[][],
+  chipSize: number
+) {
+  for (let i = 0; i < map.length; i++) {
+    for (let j = 0; j < map[i].length; j++) {
+      const chip = map[i][j];
+
+      canvas.copy(
+        texture,
+        {
+          x: (chip % 4) * chipSize,
+          y: ((chip / 4) | 0) * chipSize,
+          width: chipSize,
+          height: chipSize,
+        },
+        {
+          x: j * chipSize * 4,
+          y: i * chipSize * 4,
+          width: chipSize * 4,
+          height: chipSize * 4,
+        }
+      );
+    }
+  }
+}
+
+export class Sprite {
+  x = 0;
+  y = 0;
+  z = 0;
+  vx = 0;
+  vy = 0;
+  originX = 0;
+  originY = 0;
+  scale = 1;
+  texture: number;
+  frames: Rect[];
+  index = 0;
+
+  constructor(texture: number, frames: Rect[]) {
+    this.texture = texture;
+    this.frames = frames;
+  }
+
+  draw(dest: Canvas) {
+    dest.copy(this.texture, this.frames[this.index], {
+      x: this.x - this.originX,
+      y: this.y - this.originY - this.z,
+      width: this.frames[this.index].width * this.scale,
+      height: this.frames[this.index].height * this.scale,
+    });
+  }
+
+  tick() {
+    this.x += this.vx;
+    this.y += this.vy;
+  }
+
+  wrap(width: number, height: number) {
+    if (this.x < 0) {
+      this.x += width;
+    } else if (this.x > width) {
+      this.x -= width;
+    }
+
+    if (this.y < 0) {
+      this.y += height;
+    } else if (this.y > height) {
+      this.y -= height;
+    }
+  }
+}
